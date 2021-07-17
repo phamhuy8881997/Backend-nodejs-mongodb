21bbd93801708c949bb297cf3a26c07f375a460e
Huy <phamhuy8881997@gmail.com>
2021-07-17 15:55:42 +0700

    updata 4

--- models/dataCart.js
+++ models/dataCart.js
@@ -1,10 +1,6 @@
 var mongoose = require("mongoose");
 var DataBaseAdminCart = new mongoose.Schema({
-  user: { type: Object, required: true },
-  Cart: { type: Array, required: true },
-  MDH: { type: String, required: true },
-  status: { type: Boolean, required: true },
-  times: { type: String, required: true },
+  data: { type: Object, required: true },
 });
 // Biên dịch mô hình từ schema
 module.exports = mongoose.model("DataBaseAdminCart", DataBaseAdminCart);
--- routes/postDataCart.js
+++ routes/postDataCart.js
@@ -1,62 +1,50 @@
-var express=require('express');
-var router=express.Router();
-var DataCart=require('../models/dataCart');
+var express = require("express");
+var router = express.Router();
+var DataCart = require("../models/dataCart");
 
 // ============get api===============================
-router.get('/', async (req,res)=>{
-    try{
-        const Get_todo= await DataCart.find();
-        res.json(Get_todo);
-    }catch(err){
-        res.json({message:err});
-    }
+router.get("/", async (req, res) => {
+  try {
+    const Get_todo = await DataCart.find();
+    res.json(Get_todo);
+  } catch (err) {
+    res.json({ message: err });
+  }
 });
 // ============post api===============================
-router.post('/', async (req,res)=>{
-    var post_todo= new DataCart({
-        user: req.body.user,
-        Cart:req.body.Cart,
-        MDH: req.body.MDH,
-        status:req.body.status,
-        times:req.body.times
-    });
-    try{
-        const Post_todo = await post_todo.save();
-        res.json(Post_todo);
-    }catch(err){
-        res.json({message:err})
-    }
+router.post("/", async (req, res) => {
+  var post_todo = new DataCart({
+    data: req.body.data,
+  });
+  try {
+    const Post_todo = await post_todo.save();
+    res.json(Post_todo);
+  } catch (err) {
+    res.json({ message: err });
+  }
 });
 // ============update api============================
-router.put('/:id', async (req,res)=>{
-    try{
-        var dataPost = {
-            user: req.body.user,
-            Cart:req.body.Cart,
-            MDH: req.body.MDH,
-            status:req.body.status,
-            times:req.body.times
-        }
-        var Update_id = await DataCart.findByIdAndUpdate(req.params.id, dataPost);
-        res.json(Update_id);
-    }catch(err){
-        res.json({message:err});
-    }
+router.put("/:id", async (req, res) => {
+  try {
+    var dataPut = {
+      data: req.body.data,
+    };
+    var Update_id = await DataCart.findByIdAndUpdate(req.params.id, dataPut);
+    res.json(Update_id);
+  } catch (err) {
+    res.json({ message: err });
+  }
 });
 // ============delete api id=========================
-router.delete('/:id', async (req,res)=>{
-    try{
-        const Delete_id = await DataCart.remove({_id:req.params.id});
-        res.json(Delete_id);
-    }catch(err){
-        res.json({message:err});
-    }
+router.delete("/:id", async (req, res) => {
+  try {
+    const Delete_id = await DataCart.remove({ _id: req.params.id });
+    res.json(Delete_id);
+  } catch (err) {
+    res.json({ message: err });
+  }
 });
 //===================================================
 
-module.exports=router;
-
-
-
-
+module.exports = router;
 
